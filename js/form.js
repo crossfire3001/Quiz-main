db3e0644069f597a6f7dec7b0f6908ea13c36175
crossfire3001 <114337540+crossfire3001@users.noreply.github.com>
2024-05-02 18:43:22 +0500

    update form.js
    validation update with pure JS

--- js/form.js
+++ js/form.js
@@ -23,6 +23,9 @@
         valid: false,
       },
     ],
+    agreeElement: null,
+    processElement: null,
+
     // init function: Initializes the Form object by attaching DOM elements to each field and setting up event handlers for validation.
     // This function ensures that each input field in the form is monitored for changes, and when a change occurs, the appropriate validation function is called to check the input against specified rules.
     init() {
@@ -33,8 +36,41 @@
           that.validateField.call(that, field, this); // Use the 'onchange' event to trigger validation when the user changes the input
         };
       });
+
+      // чекбокс ознакомления с сайтом
+      this.agreeElement = document.getElementById("agree");
+      this.agreeElement.onchange = function () {
+        that.validateForm();
+      };
+
+      // кнопка "Перейти к выбору теста"
+      this.processElement = document.getElementById("process");
+      this.processElement.onchange = function () {};
     },
-    validateField(field, element) {},
+    // валидация полей
+    validateField(field, element) {
+      if (!element.value || !element.value.match(field.regex)) {
+        element.parentNode.style.borderColor = "red";
+        field.valid = false;
+      } else {
+        element.parentNode.removeAttribute("style");
+        field.valid = true;
+      }
+      this.validateForm();
+    },
+    // валидация всей формы для разблокировки кнопки "Перейти к выбору теста"
+    validateForm() {
+      // проверка каждого поля на валидность
+      const validForm = this.fields.every((field) => field.valid);
+      // если есть чекбокс чекнут и форма валидна
+      if (this.agreeElement.checked && validForm) {
+        // убираем атрибут с кнопки для нажатия
+        this.processElement.removeAttribute("disabled");
+      } else {
+        // если какое-либо поле не валидно, то обратно добавляем атрибут "disabled" на кнопку "Перейти выбору к теста"
+        this.processElement.setAttribute("disabled", "disabled");
+      }
+    },
   };
 
   Form.init();
