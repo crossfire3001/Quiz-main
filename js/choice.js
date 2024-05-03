7a09a03284ee570cc440e9aa6e2d7acdb1cc1608
crossfire3001 <114337540+crossfire3001@users.noreply.github.com>
2024-05-03 19:04:32 +0500

    update choice.js
    request from the server a list of tests using XML and JS to get the data from backend

--- js/choice.js
+++ js/choice.js
@@ -1,10 +1,35 @@
 (function () {
   const Choice = {
+    quizzes: [],
     init() {
+      checkUserData();
 
-        
+      // запрос с сервера список тестов
+      const xhr = new XMLHttpRequest();
+      // не делаем запрос асинхронно - false
+      xhr.open("GET", "https://testologia.ru/get-quizzes", false);
+      // отправляем наш запрос
+      xhr.send();
+
+      // если наш запрос успешный и там пришел какой-то ответ
+      if (xhr.status === 200 && xhr.responseText) {
+        try {
+          // нам нужно распарсить наш текст в JS объект и сохранить в this.quizzes
+          this.quizzes = JSON.parse(xhr.responseText);
+        } catch (e) {
+          // если возникла какая-то ошибка мы просто переведем пользователя на главную страницу
+          location.href = "index.html";
+        }
+        // отображаем наши результаты с бекенда
+        this.processQuizzes();
+      } else {
+        // если от сервера ответ не 200 или какой-то другой, перенаправляем пользоваля на главную страницу
+        location.href = "index.html";
+      }
     },
+    // вызов функции после получения всех необходимых данных
+    processQuizzes() {},
   };
 
   Choice.init();
-});
+})();
