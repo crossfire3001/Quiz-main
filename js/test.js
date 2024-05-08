593a495b6f0f4d20c11eadfacf63bc4b92e6ca49
crossfire3001 <114337540+crossfire3001@users.noreply.github.com>
2024-05-08 10:40:08 +0500

    update test.js
    Dynamic answers are displayed in test.html using JS

--- js/test.js
+++ js/test.js
@@ -2,6 +2,7 @@
   const Test = {
     quiz: null,
     questionTitleElement: null,
+    optionsElement: null,
     // опрос всегда должен начинатся с самого первого задания
     currentQuestionIndex: 1,
     init() {
@@ -38,6 +39,8 @@
       console.log(this.quiz);
       // берем название вопроса
       this.questionTitleElement = document.getElementById("title");
+      // берем названия ответов
+      this.optionsElement = document.getElementById("options");
 
       this.showQuestion();
     },
@@ -48,6 +51,34 @@
 
       // создание вопроса в HTML
       this.questionTitleElement.innerHTML = `<span>Вопрос ${this.currentQuestionIndex}: </span> ${activeQuestion.question}`;
+
+      // создание пуского HTML внутри .test-question-options
+      this.optionsElement.innerHTML = "";
+      // перебираем ответы
+      activeQuestion.answers.forEach((answer) => {
+        const optionElement = document.createElement("div");
+        optionElement.className = "test-question-option";
+
+        // добавляем к input ответ с id
+        const inputId = "answer-" + answer.id;
+        const inputElement = document.createElement("input");
+        inputElement.setAttribute("id", inputId);
+        inputElement.setAttribute("type", "radio");
+        inputElement.setAttribute("name", "answer");
+        inputElement.setAttribute("value", answer.id);
+
+        const labelElement = document.createElement("label");
+        labelElement.setAttribute("for", inputId);
+        // вариант ответа на вопрос внутри label
+        labelElement.innerText = answer.answer;
+
+        // помещаем внутри .test-question-option: input, label
+        optionElement.appendChild(inputElement);
+        optionElement.appendChild(labelElement);
+
+        // помещаем внутри .test-question-options: .test-question-option
+        this.optionsElement.appendChild(optionElement);
+      });
     },
   };
 
